b35cfdbfbb845fbe01819dc874806516db3e1e8a
SivalankaLaxmi <sivalankalaxmi809@gmail.com>
2026-05-18 13:45:49 +0530

    URLs added and opening the required websites

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import time
 
-recognizer = sr.Recognizer()
+r = sr.Recognizer()
 engine = pyttsx3.init()
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processCommand(c):
+    if "open google" in c.lower():
+        print("Google is opening")
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        print("Facebook is opening")
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        print("Youtube is opening")
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        print("LinkedIn is opening")
+        webbrowser.open("https://linkedin.com")
+    else:
+        speak("Command not recognized")
+
 if __name__ == "__main__":
-    speak("Initializing Jarivis....")
+    speak("Initializing Jarvis....")
     while True:
     #Listen for the wake word "Jarivis"
     # obtain audio from the microphone
 
-        r = sr.Recognizer()
+        # r = sr.Recognizer()
     
 
         # command = r.recognize_sphinx(audio)
@@ -24,13 +41,26 @@ if __name__ == "__main__":
         print("recognizing...")
         try:
             with sr.Microphone() as source:
-                print("Listening")
-                audio = r.listen(source, timeout=2)
-            command = r.recognize_google(audio)
-            print(command)
-            if(command.lower() == "jarvis"):
-                speak("Ya")
-                # listen for command
+                print("Listening for wake word...")
+                r.adjust_for_ambient_noise(source)
+                audio = r.listen(source)
+                word = r.recognize_google(audio)
+                print(repr(word))
+                print("You said: ", word)
+             
+                if "jarvis" in word.lower():
+                    print(word)
+                    speak("Ya")
+                    time.sleep(1)
+                    # listen for command
+                    with sr.Microphone() as source:
+                        print("Jarvis Active...")
+                        audio = r.listen(source)
+                        command = r.recognize_google(audio)
+                        print(repr(command))
+                        print("command ---", command)
+                        processCommand(command)
+
         except Exception as e:
             print("Error; {0}".format(e))
 
